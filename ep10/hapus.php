4bed9ba6ae9e4f3cd5588ce17aa6f4bdd5003f07
prasojowahyu <64030808+prasojowahyu@users.noreply.github.com>
2020-05-11 09:12:38 +0700

    Adding @5 and note @6
    @5 connecting var $_GET from index.php to hapus.php
    @6 add note

--- ep10/hapus.php
+++ ep10/hapus.php
@@ -2,20 +2,22 @@
     require 'functions.php';
 
     //konekin ke halaman index
-    $idmov = $_GET["idmovie"];
-    if ( hapus($idmov) > 0) {
-            echo "
-                <script>
-                    alert('data berhasil dihapus.');
-                    document.location.href = 'index.php';
-                </script>
-            ";
-        } else {
-            echo "
-                <script>
-                    alert('data gagal dihapus.');
-                    document.location.href = 'index.php';
-                </script>
-            ";
-        }
+    if(isset($_GET["idmovie"]) ) {
+        //cek keberhasilan querynya
+        if ( $_GET["idmovie"]) > 0) {
+                echo "
+                    <script>
+                        alert('data berhasil dihapus.');
+                        document.location.href = 'index.php';
+                    </script>
+                ";
+            } else {
+                echo "
+                    <script>
+                        alert('data gagal dihapus.');
+                        document.location.href = 'index.php';
+                    </script>
+                ";
+            }
+    }
 ?>
