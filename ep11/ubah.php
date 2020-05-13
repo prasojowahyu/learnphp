2ad72b70eeba41ad0842e02b62091e97b62f8400
Wahyu Prasojo <wahyuprasojo@students.unnes.ac.id>
2020-05-13 09:28:31 +0700

    add query php and insert form value

--- ep11/ubah.php
+++ ep11/ubah.php
@@ -1,10 +1,15 @@
 <?php
 
 require 'functions.php';
+//ambil data dari url index.php
+$idmovie = $_GET["idmovie"];
+
+//query data movie berdasarkan idnya
+$mov = query("SELECT * FROM movietab WHERE idmovie = $idmovie")[0]; //[0] berarti setelah panggil fn query begitu masuk ke array $rows, langsung cari row ke 0.
 
 //submit check
 if (isset($_POST["submit"])) {
-    
+
     //cek data udh ditambah blm
     if (ubahdata($_POST) > 0) {
         echo "
@@ -40,32 +45,32 @@ if (isset($_POST["submit"])) {
         <ul>
             <li>
                 <label for="judul">Judul:</label>
-                <input type="text" name="judul" id="judul" required>
+                <input type="text" name="judul" id="judul" value="<?= $mov["judul"]; ?>" required>
             </li>
             <li>
                 <label for="imdb_rating">IMDB Rating:</label>
-                <input type="text" name="imdb_rating" id="imdb_rating" required>
+                <input type="text" name="imdb_rating" id="imdb_rating" value="<?= $mov["imdb_rating"]; ?>" required>
             </li>
             <li>
                 <label for="tahun">Tahun:</label>
-                <input type="text" name="tahun" id="tahun" required>
+                <input type="text" name="tahun" id="tahun" value="<?= $mov["tahun"]; ?>" required>
             </li>
             <li>
-                <label for="genre">Genre:</label>
-                <input type="text" name="genre" id="genre" required>
+                <label for=" genre">Genre:</label>
+                <input type="text" name="genre" id="genre" value="<?= $mov["genre"]; ?>" required>
             </li>
             <li>
-                <label for="director">Director:</label>
-                <input type="text" name="director" id="director" required>
+                <label for=" director">Director:</label>
+                <input type="text" name="director" id="director" value="<?= $mov["director"]; ?>" required>
             </li>
             <li>
-                <label for="animasi">Animasi: </label>
-                <input type="radio" name="animasi" id="animasi" value="1" checked="checked">Ya
+                <label for="animasi" value="<?= $mov["animasi"]; ?>">Animasi: </label>
+                <input type=" radio" name="animasi" id="animasi" value="1" checked="checked">Ya
                 <input type="radio" name="animasi" id="animasi" value="0">Tidak
             </li>
             <li>
                 <label for="poster">Poster </label>
-                <input type="text" name="poster" id="poster">
+                <input type="text" name="poster" id="poster" value="<?= $mov["director"]; ?>">
             </li>
             <br>
             <li>
