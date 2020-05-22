5a5a8477c76e8f47a0c0c9734bbaf2fd3c77fdcd
Wahyu Prasojo <wahyuprasojo@students.unnes.ac.id>
2020-05-22 09:11:59 +0700

    koneksi regist ke functions, sekalian buat alert

--- ep14/regist.php
+++ ep14/regist.php
@@ -1,8 +1,24 @@
+<?php 
+    require 'functions.php';
+
+    //cek tombol daftar, kalo ditekan, jalankan ini
+    if ( isset($_POST["register"]) ) {
+        if ( registrasi($_POST) > 0 ) {
+            echo    "<script>
+                        alert('User baru berhasil ditambahkan!');
+                    </script>";
+        } else {
+            echo mysqli_error($dbconn);
+        }
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>Halaman Registerasi</title>
+        <title>Halaman Registrasi</title>
     </head>
     <style>
         label {
@@ -12,7 +28,7 @@
 
     <body>
         
-        <h1>Registerasi Akun</h1>
+        <h1>Registrasi Akun</h1>
 
         <form method="post" action="">
             <li>
@@ -28,7 +44,7 @@
                 <input type="password" name="password2" id="password2">
             </li>
             <li>
-                <button type="submit" name="registerasi">Daftar!</button>
+                <button type="submit" name="register">Daftar!</button>
             </li>
 
         </form>
