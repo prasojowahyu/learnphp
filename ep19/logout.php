d07ce4e60ffba971871b8a73ce26f7e501980c63
Wahyu Prasojo <wahyuprasojo@students.unnes.ac.id>
2020-06-23 06:17:55 +0700

    delete ep19

--- ep19/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    session_start();                   //  \
-    $_SESSION = [];                    //   \
-    session_unset();                   //   / LOGOUT FROM SESSION||;
-    session_destroy();                 //  /
-    header( "Location: login.php" );   // /
-?>
